5789efb60a04b3029fae10f7cada57f43896655f
azidfadhil <mail.azidfadhil@gmail.com>
2026-05-08 18:04:52 +0700

    fix: correct typo in greeting message of API response

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { Elysia } from "elysia";
 
 const app = new Elysia()
   .get("/", () => ({
-    message: "Haloow! Ini adalah API ElysiaJS dengan bahasa Typescript!",
+    message: "Haloowww! Ini adalah API ElysiaJS dengan bahasa Typescript!",
   }))
   .listen(3000);
 
