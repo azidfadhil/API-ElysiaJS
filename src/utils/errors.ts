69ffe43a2a58c2ab41b2b3e424cebc2733bc9ad4
Zeddd. <mail.azidfadhil@gmail.com>
2026-05-11 17:11:38 +0700

    feat: add custom error classes for improved error handling

---
+++ src/utils/errors.ts
@@ -0,0 +1,33 @@
+export class AppError extends Error {
+  constructor(
+    public statusCode: number,
+    message: string
+  ) {
+    super(message)
+    this.name = this.constructor.name
+  }
+}
+
+export class NotFoundError extends AppError {
+  constructor(message = "Resource not found") {
+    super(404, message)
+  }
+}
+
+export class ConflictError extends AppError {
+  constructor(message = "Resource already exists") {
+    super(409, message)
+  }
+}
+
+export class BadRequestError extends AppError {
+  constructor(message = "Bad request") {
+    super(400, message)
+  }
+}
+
+export class UnauthorizedError extends AppError {
+  constructor(message = "Unauthorized") {
+    super(401, message)
+  }
+}
